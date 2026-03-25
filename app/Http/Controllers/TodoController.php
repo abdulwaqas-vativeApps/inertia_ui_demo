c6135d4cc8cb4a6bc317abfc46037b1bcb7bd3fb
Abdul Waqas <abdul.waqas@vativeapps.com>
2026-03-25 11:40:24 +0500

    add commits

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+// namespace = folder
+namespace App\Http\Controllers; // tells file directory/location, to locate class
 
-use App\Models\Todo;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
+// use = import
+
+use App\Models\Todo; // todo Model to communicante db
+use Illuminate\Http\Request; // for handle user request data
+use Inertia\Inertia; // for laravel react connect
+
+
+// extends means => TodoController class extends to Controller(base/parent) class, so that it can get Controller class feature
 
 class TodoController extends Controller
 {
@@ -28,24 +34,43 @@ class TodoController extends Controller
         return redirect()->back();
     }
 
-public function update(Request $request, Todo $todo)
-{
-    $data = $request->validate([
-        'title' => 'nullable|string',
-        'completed' => 'nullable|boolean',
-    ]);
+    public function update(Request $request, Todo $todo)
+    {
+        $data = $request->validate([
+            'title' => 'nullable|string',
+            'completed' => 'nullable|boolean',
+        ]);
 
-    $todo->update([
-        'title' => $data['title'] ?? $todo->title,
-        'completed' => $data['completed'] ?? $todo->completed,
-    ]);
+        $todo->update([
+            'title' => $data['title'] ?? $todo->title,
+            'completed' => $data['completed'] ?? $todo->completed,
+        ]);
 
-    return redirect('/todos', 303);
-}
+        return redirect('/todos', 303);
+    }
     public function destroy(Todo $todo)
     {
         $todo->delete();
 
-      return redirect('/todos', 303);   
- }
+        return redirect('/todos', 303);
+    }
 }
+
+/*
+Todo (class) :: = poori table, class level
+$todo (object) -> = ek row, object level
+
+
+| Part    | Meaning                  |
+| ------- | ------------------------ |
+| `Todo`  | model (DB table)         |
+| `$todo` | variable (single record) |
+
+
+
+| Part       | Meaning              |
+| ---------- | -------------------- |
+| `Request`  | type (Laravel class) |
+| `$request` | variable (user data) |
+
+*/
